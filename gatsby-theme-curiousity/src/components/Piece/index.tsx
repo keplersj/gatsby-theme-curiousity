68e2dbec0abe62475fcaf2ccb08633b1cdd94bf8
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-12-23 20:36:16 -0700

    fix: remove old metadata

--- gatsby-theme-curiousity/src/components/Piece/index.tsx
+++ gatsby-theme-curiousity/src/components/Piece/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "@emotion/styled";
 import BaseLayout from "../Base";
-import { Helmet } from "react-helmet";
 import { MDXRenderer } from "gatsby-plugin-mdx";
 
 const Content = styled.div`
@@ -32,58 +31,15 @@ interface Props {
 const ProjectPageTemplate = ({
   data: { portfolioItem: piece }
 }: Props): React.ReactElement<Props> => (
-  <>
-    <Helmet>
-      <script type="application/ld+json">
-        {JSON.stringify({
-          "@context": "http://www.schema.org",
-          "@type": "WebPage",
-          name: `${piece.title} | Kepler Sticka-Jones`,
-          description: piece.excerpt,
-          url: piece.slug,
-          breadcrumb: {
-            "@type": "BreadcrumbList",
-            itemListElement: [
-              {
-                "@type": "ListItem",
-                position: 1,
-                item: {
-                  "@id": "https://keplersj.com/",
-                  name: "Kepler Sticka-Jones"
-                }
-              },
-              {
-                "@type": "ListItem",
-                position: 2,
-                item: {
-                  "@id": "https://keplersj.com/projects/",
-                  name: "Projects"
-                }
-              },
-              {
-                "@type": "ListItem",
-                position: 3,
-                item: {
-                  "@id": `https://keplersj.com${piece.slug}`,
-                  name: piece.title
-                }
-              }
-            ]
-          }
-        })}
-      </script>
-    </Helmet>
-
-    <BaseLayout title={piece.title} description={piece.excerpt}>
-      <Content>
-        <h1>{piece.title}</h1>
-        {/* <br /> */}
-        <section>
-          <MDXRenderer>{piece.body}</MDXRenderer>
-        </section>
-      </Content>
-    </BaseLayout>
-  </>
+  <BaseLayout title={piece.title} description={piece.excerpt}>
+    <Content>
+      <h1>{piece.title}</h1>
+      {/* <br /> */}
+      <section>
+        <MDXRenderer>{piece.body}</MDXRenderer>
+      </section>
+    </Content>
+  </BaseLayout>
 );
 
 export default ProjectPageTemplate;
--- gatsby-theme-curiousity/src/components/Portfolio/index.tsx
+++ gatsby-theme-curiousity/src/components/Portfolio/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Helmet } from "react-helmet";
 import styled from "@emotion/styled";
 import BaseLayout from "../Base";
 import { PortfolioListItem as Project } from "../PortfolioListItem";
@@ -32,71 +31,23 @@ interface Props {
 }
 
 const ProjectsPage = ({ data }: Props): React.ReactElement<Props> => (
-  <>
-    <Helmet>
-      <script type="application/ld+json">
-        {JSON.stringify({
-          "@context": "http://www.schema.org",
-          "@type": "CollectionPage",
-          name: "Projects | Kepler Sticka-Jones",
-          url: "/projects",
-          breadcrumb: {
-            "@type": "BreadcrumbList",
-            itemListElement: [
-              {
-                "@type": "ListItem",
-                position: 1,
-                item: {
-                  "@id": "https://keplersj.com/",
-                  name: "Kepler Sticka-Jones"
-                }
-              },
-              {
-                "@type": "ListItem",
-                position: 2,
-                item: {
-                  "@id": "https://keplersj.com/projects/",
-                  name: "Projects"
-                }
-              }
-            ]
-          },
-          about: {
-            "@type": "ItemList",
-            name: "Projects | Kepler Sticka-Jones",
-            url: "/projects",
-            numberOfItems: data.allPortfolioItem.edges.length,
-            itemListElement: data.allPortfolioItem.edges.map(
-              ({ node }): object => ({
-                "@type": "Thing",
-                name: node.title,
-                description: node.excerpt,
-                url: node.slug
-              })
-            )
-          }
-        })}
-      </script>
-    </Helmet>
-
-    <BaseLayout title="Projects">
-      <Projects>
-        <h1>Projects</h1>
-        <ListContainer>
-          {data.allPortfolioItem.edges.map(
-            ({ node }): React.ReactElement => (
-              <Project
-                key={node.id}
-                location={node.slug}
-                title={node.title}
-                description={node.excerpt}
-              />
-            )
-          )}
-        </ListContainer>
-      </Projects>
-    </BaseLayout>
-  </>
+  <BaseLayout title="Projects">
+    <Projects>
+      <h1>Projects</h1>
+      <ListContainer>
+        {data.allPortfolioItem.edges.map(
+          ({ node }): React.ReactElement => (
+            <Project
+              key={node.id}
+              location={node.slug}
+              title={node.title}
+              description={node.excerpt}
+            />
+          )
+        )}
+      </ListContainer>
+    </Projects>
+  </BaseLayout>
 );
 
 export default ProjectsPage;

9fe5167c9981635f192153f9c74d90215d5079f2
Kepler Sticka-Jones <kepler+git@stickajones.org>
2020-01-13 20:03:04 -0700

    fix(Portfolio): properly use new Card component

--- gatsby-theme-curiousity/src/components/PortfolioListItem/index.tsx
+++ gatsby-theme-curiousity/src/components/PortfolioListItem/index.tsx
@@ -13,38 +13,39 @@ interface Props {
   image?: FluidObject;
 }
 
+const StyledCard = styled(Card)`
+  @media screen and (min-width: 1024px) {
+    min-height: 192px;
+  }
+
+  @media screen and (max-width: 512px) {
+    max-width: 100vw;
+  }
+
+  @media screen and (min-width: 768px) {
+    max-width: 33vw;
+  }
+
+  @media screen and (min-width: 1024px) {
+    max-width: 25vw;
+  }
+`;
+
 const Container = styled.article`
   display: flex;
   flex-direction: column;
-  border: 1px solid;
-  border-radius: 5px;
-  width: 20em;
-  margin: 0.5em;
-  background-color: #fafafa;
-
-  @media (max-width: 35em) {
-    flex-grow: 1;
-  }
-
-  @media (prefers-color-scheme: dark) {
-    background-color: #282c34;
-  }
-
-  @media screen and (min-width: 512px) {
-    min-height: 192px;
-  }
 `;
 
 const FeaturedImage = styled.figure`
   height: 100%;
-  min-width: 256px;
+  width: 100%;
   @media screen and (max-width: 321px) {
     display: none;
   }
 `;
 
-const Info = styled(Card)`
-  margin: 1rem;
+const Info = styled.div`
+  padding: 1rem;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -62,49 +63,47 @@ export const PortfolioListItem = (props: Props): React.ReactElement<Props> => {
   `);
 
   return (
-    <Container>
-      <JsonLd<CreativeWork>
-        item={{
-          "@context": "https://schema.org",
-          "@type": "CreativeWork",
-          "@id": `${data.site.siteMetadata.siteUrl}${props.location}`,
-          url: `${data.site.siteMetadata.siteUrl}${props.location}`,
-          headline: props.title,
-          name: props.title,
-          mainEntityOfPage: `${data.site.siteMetadata.siteUrl}${props.location}`,
-          image: props.image && {
-            "@type": "ImageObject",
-            "@id": `${data.site.siteMetadata.siteUrl}${props.image.src}`
-          }
-        }}
-      />
-      {props.image && (
-        <Link to={props.location}>
-          <FeaturedImage>
-            <JsonLd<ImageObject>
-              item={{
-                "@context": "https://schema.org",
-                "@type": "ImageObject",
-                "@id": `${data.site.siteMetadata.siteUrl}${props.image.src}`,
-                representativeOfPage: false,
-                contentUrl: props.image.src,
-                url: props.image.src
-              }}
-            />
-            <Image fluid={props.image} />
-          </FeaturedImage>
-        </Link>
-      )}
-      <Info
-        title={props.title}
-        location={props.location}
-        customLinkComponent={(title, location): React.ReactElement => (
-          <Link to={location}>
-            <h2>{title}</h2>
+    <StyledCard>
+      <Container>
+        <JsonLd<CreativeWork>
+          item={{
+            "@context": "https://schema.org",
+            "@type": "CreativeWork",
+            "@id": `${data.site.siteMetadata.siteUrl}${props.location}`,
+            url: `${data.site.siteMetadata.siteUrl}${props.location}`,
+            headline: props.title,
+            name: props.title,
+            mainEntityOfPage: `${data.site.siteMetadata.siteUrl}${props.location}`,
+            image: props.image && {
+              "@type": "ImageObject",
+              "@id": `${data.site.siteMetadata.siteUrl}${props.image.src}`
+            }
+          }}
+        />
+        {props.image && (
+          <Link to={props.location}>
+            <FeaturedImage>
+              <JsonLd<ImageObject>
+                item={{
+                  "@context": "https://schema.org",
+                  "@type": "ImageObject",
+                  "@id": `${data.site.siteMetadata.siteUrl}${props.image.src}`,
+                  representativeOfPage: false,
+                  contentUrl: props.image.src,
+                  url: props.image.src
+                }}
+              />
+              <Image fluid={props.image} />
+            </FeaturedImage>
           </Link>
         )}
-        supporting={<div>{props.description}</div>}
-      />
-    </Container>
+        <Info>
+          <Link to={props.location}>
+            <h2>{props.title}</h2>
+          </Link>
+          <p>{props.description}</p>
+        </Info>
+      </Container>
+    </StyledCard>
   );
 };
